2c9579c0e47f315d8eb419f08aa771ac7cbfb7f9
Ananya Tripathi <Ananya.Tripathi@Geminisolutions.com>
2023-08-01 10:39:21 +0530

    second commit

--- app.js
+++ app.js
@@ -2,32 +2,36 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    fs.readFile('text.json', 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-        }
-        else {
-            obj = {
-                data:JSON.parse(data)
+    if(req.method=='GET')
+    {
+        fs.readFile('text.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
             }
-            //console.log(obj);
-
-            fs.writeFile('data.txt', JSON.stringify(obj), (err) => {
-                if (err) {
-                    console.error(err);
+            else {
+                obj = {
+                    data:JSON.parse(data)
                 }
-                else {
-                    res.writeHead(200, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify(obj));
-                }
-            })
-        }
-    })
+                //console.log(obj);
+    
+                fs.writeFile('data.txt', JSON.stringify(obj), (err) => {
+                    if (err) {
+                        console.error(err);
+                    }
+                    else {
+                        res.writeHead(200, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify(obj));
+                    }
+                })
+            }
+        })
+    }
+   
 
 })
 
 
 
-server.listen(8080, () => {
+server.listen(8090, () => {
     console.log('success');
 });
